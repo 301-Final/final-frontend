7d7a79d90d21dfb192eb2d25e46692aa63dc9f4d
Michael Dulin <michaeldulin1211@gmail.com>
2023-03-21 13:47:47 -0700

    Added Header, Footer, Login, Profile and About

--- src/components/About.js
+++ src/components/About.js
@@ -1,20 +1,67 @@
 import React from "react";
-import ListGroup from 'react-bootstrap/ListGroup'
+import { Tabs, Tab, Modal, Card } from 'react-bootstrap'
 
 class About extends React.Component {
 
   render() {
-
     return (
-      
-        <ListGroup>
-          <ListGroup.Item variant="warning">About us:</ListGroup.Item>
-          <ListGroup.Item variant="info">Thomas Lau</ListGroup.Item>
-          <ListGroup.Item>Full stack developer</ListGroup.Item>
-          <ListGroup.Item variant="info">Kirill Lesnykh</ListGroup.Item>
-          <ListGroup.Item>Full stack developer</ListGroup.Item>
-        </ListGroup>
+      <Tabs
+        defaultActiveKey="aboutus"
+        id="tabber"
+        className="mb-3"
+        justify
+      >
+        <Tab eventKey="aboutus" title="About Us">
+          <body>
+            <Card>
+              <article>
+                <img
+                  src="./images/mike.jpg"
+                  alt="Mike"
+                  onClick={this.handleImgClick}
+                  style={{ maxHeight: '18rem', maxWidth: '18rem' }}
+                />
+                <h2>Michael Dulin</h2>
+              </article>
+            </Card>
+            <Card>
+              <article>
+                <img
+                  src="./images/cody.jpg"
+                  alt="Cody"
+                  onClick={this.handleImgClick}
+                  style={{ maxHeight: '18rem', maxWidth: '18rem' }}
+                />
+                <h2>Cody Lobdell</h2>
+              </article>
+            </Card>
+            <Card>
+              <article>
+                <img
+                  src="./images/kirill.jpg"
+                  alt="Mike"
+                  onClick={this.handleImgClick}
+                  style={{ maxHeight: '18rem', maxWidth: '18rem' }}
+                />
+                <h2>Kirill Lesnykh</h2>
+              </article>
+            </Card>
+          </body>
+        </Tab>
+        <Tab eventKey="tools" title="Tools">
+          <body>
 
+          </body>
+        </Tab>
+        <Tab eventKey="mission" title="Mission" disabled>
+          <body>
+            <h2>Our Mission</h2>
+            <p>Our mission our mission our mission our mission. Our mission our mission our mission our mission. Our mission our mission our mission our mission. Our mission our mission our mission our mission. Our mission our mission our mission our mission. Our mission our mission our mission our mission. </p>
+          </body>
+        </Tab>
+      </Tabs>
     )
   }
 };
+
+export default About;
