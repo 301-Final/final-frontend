569f2c68ed936212998e3cc85a5d60c73d6e030e
MichaelDulin <michaeldulin1211@gmail.com>
2023-03-23 22:53:32 +0000

    divergent branch - DELETE

--- src/components/About.js
+++ src/components/About.js
@@ -1,67 +1,67 @@
 import React from "react";
-import { Tabs, Tab, Card } from 'react-bootstrap'
+import { Tabs, Tab, Card } from "react-bootstrap";
+import About from "./About.css"
 
 class About extends React.Component {
-
   render() {
     return (
-      <Tabs
-        defaultActiveKey="aboutus"
-        id="tabber"
-        className="mb-3"
-        justify
-      >
+      <Tabs defaultActiveKey="aboutus" id="tabber" className="mb-3" justify>
         <Tab eventKey="aboutus" title="About Us">
-          <body>
             <Card>
-              <article>
-                <img
-                  src="./images/mike.jpg"
-                  alt="Mike"
-                  onClick={this.handleImgClick}
-                  style={{ maxHeight: '18rem', maxWidth: '18rem' }}
-                />
-                <h2>Michael Dulin</h2>
-              </article>
+              <Card.Title>Michael Dulin</Card.Title>
+              <Card.Img
+                src="./images/mike.jpg"
+                alt="Mike"
+                style={{ maxHeight: "18rem", maxWidth: "18rem" }}
+              />
+              <Card.Text>Michael likes to do stuff</Card.Text>
             </Card>
             <Card>
-              <article>
-                <img
-                  src="./images/cody.jpg"
-                  alt="Cody"
-                  onClick={this.handleImgClick}
-                  style={{ maxHeight: '18rem', maxWidth: '18rem' }}
-                />
-                <h2>Cody Lobdell</h2>
-              </article>
+            <Card.Title>Cody Lobdell</Card.Title>
+              <Card.Img
+                src="./images/cody.jpg"
+                alt="Cody"
+                style={{ maxHeight: "18rem", maxWidth: "18rem" }}
+              />
+              <Card.Text>Cody likes to do stuff</Card.Text>
             </Card>
             <Card>
-              <article>
-                <img
-                  src="./images/kirill.jpg"
-                  alt="Mike"
-                  onClick={this.handleImgClick}
-                  style={{ maxHeight: '18rem', maxWidth: '18rem' }}
-                />
-                <h2>Kirill Lesnykh</h2>
-              </article>
+              <Card.Title>Kirill Lesnykh</Card.Title>
+              <Card.Img
+                src="./images/kirill.jpg"
+                alt="Mike"
+                style={{ maxHeight: "18rem", maxWidth: "18rem" }}
+              />
+              <Card.Text>Kirill does NOT like to do stuff</Card.Text>
             </Card>
-          </body>
         </Tab>
         <Tab eventKey="tools" title="Tools">
           <body>
-
+            <ul>
+              <li>JavaScript</li>
+              <li>HMTL5</li>
+              <li>CSS3</li>
+              <li>React</li>
+              <li>Mongodb</li>
+              <li>Bootstrap</li>
+            </ul>
           </body>
         </Tab>
-        <Tab eventKey="mission" title="Mission" disabled>
+        <Tab eventKey="mission" title="Mission">
           <body>
             <h2>Our Mission</h2>
-            <p>Our mission our mission our mission our mission. Our mission our mission our mission our mission. Our mission our mission our mission our mission. Our mission our mission our mission our mission. Our mission our mission our mission our mission. Our mission our mission our mission our mission. </p>
+            <p>
+              Our mission our mission our mission our mission. Our mission our
+              mission our mission our mission. Our mission our mission our
+              mission our mission. Our mission our mission our mission our
+              mission. Our mission our mission our mission our mission. Our
+              mission our mission our mission our mission.{" "}
+            </p>
           </body>
         </Tab>
       </Tabs>
-    )
+    );
   }
-};
+}
 
 export default About;
