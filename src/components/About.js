333253e5377e6ccf20cc649868e25c5ea952bfa0
Michael Dulin <michaeldulin1211@gmail.com>
2023-03-23 10:31:39 -0700

    about page structured git push origin mike-edit

--- src/components/About.js
+++ src/components/About.js
@@ -1,75 +1,79 @@
 import React from "react";
-import { Tabs, Tab, Card } from 'react-bootstrap'
+import { Carousel } from 'react-bootstrap'
 
 class About extends React.Component {
-
   render() {
     return (
-      <Tabs
-        defaultActiveKey="aboutus"
-        id="tabber"
-        className="mb-3"
-        justify
-      >
-        <Tab eventKey="aboutus" title="About Us">
-          <body>
-            <Card>
-              <article>
-              <h2>Michael Dulin</h2>
-                <img
-                  src="./images/mike.jpg"
-                  alt="Mike"
-                  onClick={this.handleImgClick}
-                  style={{ maxHeight: '18rem', maxWidth: '18rem' }}
-                />
+      <>
+        <article className="mission">
+          <h2>Our Mission</h2>
+          <p>Our mission our mission our mission our mission. Our mission our mission our mission our mission. Our mission our mission our mission our mission. Our mission our mission our mission our mission. Our mission our mission our mission our mission. Our mission our mission our mission our mission. </p>
+        </article>
+        <article className="team">
+          <h2>The Crew</h2>
+          <Carousel>
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                src="./images/mike.jpg"
+                alt="Mike"
+              />
+              <Carousel.Caption>
+                <h2>Michael Dulin</h2>
                 <p>Michael likes to do stuff</p>
-              </article>
-            </Card>
-            <Card>
-              <article>
+              </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                src="./images/cody.jpg"
+                alt="Cody"
+              />
+              <Carousel.Caption>
                 <h2>Cody Lobdell</h2>
-                <img
-                  src="./images/cody.jpg"
-                  alt="Cody"
-                  onClick={this.handleImgClick}
-                  style={{ maxHeight: '18rem', maxWidth: '18rem' }}
-                />
                 <p>Cody likes to do stuff</p>
-              </article>
-            </Card>
-            <Card>
-              <article>
+              </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                src="./images/kirill.jpg"
+                alt="Kirill"
+              />
+              <Carousel.Caption>
                 <h2>Kirill Lesnykh</h2>
-                <img
-                  src="./images/kirill.jpg"
-                  alt="Mike"
-                  onClick={this.handleImgClick}
-                  style={{ maxHeight: '18rem', maxWidth: '18rem' }}
-                />
                 <p>Kirill does NOT like to do stuff</p>
-              </article>
-            </Card>
-          </body>
-        </Tab>
-        <Tab eventKey="tools" title="Tools">
-          <body>
-            <ul>
-              <li>JavaScript</li>
-              <li>HTML5</li>
-              <li>CSS3</li>
-              <li>React</li>
-              <li>Mongodb</li>
-              <li>Bootstrap</li>
-            </ul>
-          </body>
-        </Tab>
-        <Tab eventKey="mission" title="Mission" disabled>
-          <body>
-            <h2>Our Mission</h2>
-            <p>Our mission our mission our mission our mission. Our mission our mission our mission our mission. Our mission our mission our mission our mission. Our mission our mission our mission our mission. Our mission our mission our mission our mission. Our mission our mission our mission our mission. </p>
-          </body>
-        </Tab>
-      </Tabs>
+              </Carousel.Caption>
+            </Carousel.Item>
+          </Carousel>
+        </article>
+        <article className="tools">
+          <img
+            src="./images/javascript.jpg"
+            alt="javascript"
+          />
+          <img
+            src="./images/css3.jpg"
+            alt="css3"
+          />
+          <img
+            src="./images/html5.jpg"
+            alt="html5"
+          />
+          <img
+            src="./images/mongodb.jpg"
+            alt="mongodb"
+          />
+          <img
+            src="./images/bootstrap.jpg"
+            alt="bootstrap"
+          />
+          <img
+            src="./images/github.jpg"
+            alt="github"
+          />
+        </article>
+      </>
     )
   }
 };
