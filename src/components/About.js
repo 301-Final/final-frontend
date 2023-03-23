c8c8f93c89ee8fbb0b0b0a7e92fb66ca44493711
Michael Dulin <michaeldulin1211@gmail.com>
2023-03-23 09:24:26 -0700

    styling about.js

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Tabs, Tab, Modal, Card } from 'react-bootstrap'
+import { Tabs, Tab, Card } from 'react-bootstrap'
 
 class About extends React.Component {
 
@@ -15,42 +15,52 @@ class About extends React.Component {
           <body>
             <Card>
               <article>
+              <h2>Michael Dulin</h2>
                 <img
                   src="./images/mike.jpg"
                   alt="Mike"
                   onClick={this.handleImgClick}
                   style={{ maxHeight: '18rem', maxWidth: '18rem' }}
                 />
-                <h2>Michael Dulin</h2>
+                <p>Michael likes to do stuff</p>
               </article>
             </Card>
             <Card>
               <article>
+                <h2>Cody Lobdell</h2>
                 <img
                   src="./images/cody.jpg"
                   alt="Cody"
                   onClick={this.handleImgClick}
                   style={{ maxHeight: '18rem', maxWidth: '18rem' }}
                 />
-                <h2>Cody Lobdell</h2>
+                <p>Cody likes to do stuff</p>
               </article>
             </Card>
             <Card>
               <article>
+                <h2>Kirill Lesnykh</h2>
                 <img
                   src="./images/kirill.jpg"
                   alt="Mike"
                   onClick={this.handleImgClick}
                   style={{ maxHeight: '18rem', maxWidth: '18rem' }}
                 />
-                <h2>Kirill Lesnykh</h2>
+                <p>Kirill does NOT like to do stuff</p>
               </article>
             </Card>
           </body>
         </Tab>
         <Tab eventKey="tools" title="Tools">
           <body>
-
+            <ul>
+              <li>JavaScript</li>
+              <li>HTML5</li>
+              <li>CSS3</li>
+              <li>React</li>
+              <li>Mongodb</li>
+              <li>Bootstrap</li>
+            </ul>
           </body>
         </Tab>
         <Tab eventKey="mission" title="Mission" disabled>
